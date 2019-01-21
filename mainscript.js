b5ee5182507ee3a2a8f005b0dcf07eb61bdfb06c
santhosh268842 <42665781+santhosh268842@users.noreply.github.com>
2019-01-22 00:30:47 +0530

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,4 +1,6 @@
 <script> var event = "welcome to techinical event"; function func1() { return
-(event); } console.log(func1()); </script> <script> var asthra =
-"rot13:nfg{wnin_sha}"; function func() { return (asthra); } console.log(func());
-</script>
+(event); } console.log(func1()); </script> <script>
+	 var str = "rot13:nfg{wnin_sha}";
+	function func() { return (str); }
+	console.log(func());
+	</script>
